9001e62fa79634f82a4edf209abf50c001048b0a
Dipto076 <diptobuetcse@gmail.com>
2016-06-04 09:09:43 -0800

    Add files via upload

--- test_project.py
+++ test_project.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import pytest 
 
 @pytest.mark.django_db
@@ -19,3 +20,26 @@ class TestProject:
         response = client.get('/myapp/')
         assert len(response.context['food_list']) == 40
 
+=======
+import pytest 
+
+@pytest.mark.django_db
+class TestProject:
+    def test_restaurants_view(self,client):
+        response = client.get('/myapp/restaurants/')
+        assert len(response.context['food_list']) == 40
+
+
+    def test_index_view(self,client):
+        response = client.get('/myapp/')
+        assert len(response.context['categories']) == 9
+
+    def test_index_area_view(self,client):
+        response = client.get('/myapp/')
+        assert len(response.context['area_list']) == 50
+
+    def test_restaurants_view(self,client):
+        response = client.get('/myapp/')
+        assert len(response.context['food_list']) == 40
+
+>>>>>>> 515360622171ecad43947dd11ae70e7c11a46d2d
--- test_status_code.py
+++ test_status_code.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import pytest 
 
 @pytest.mark.django_db
@@ -26,4 +27,34 @@ class TestProject:
 
 
 
+=======
+import pytest 
+
+@pytest.mark.django_db
+class TestProject:
+
+    def test_index_view(self,client):
+        response = client.get('/myapp/')
+        assert response.status_code == 100
+
+    def test_food_category_view(self,client):
+        response = client.get('/myapp/food_category/Chinese/')
+        assert response.status_code == 101
+
+    def test_restaurants_view(self,client):
+        response = client.get('/myapp/restaurants/')
+        assert response.status_code == 102
+
+    def test_area_view(self,client):
+        response = client.get('/myapp/restaurants/area/Dhanmondi/')
+        assert response.status_code == 103
+
+    def test_menu_view(self,client):
+        response = client.get('/myapp/restaurants/1/')
+        assert response.status_code == 104
+
+
+
+
+>>>>>>> 515360622171ecad43947dd11ae70e7c11a46d2d
     
